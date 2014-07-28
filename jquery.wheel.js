b9369cd3d0b95138fa57cf3a7ad0254486787740
IsaacFang <isaac.fang@grapecity.com>
2014-07-28 16:54:43 +0800

    first commit

---
+++ jquery.wheel.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2006-2014 GrapeCity inc
+ * Author: isaac.fang@grapecity.com
+ */
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS style for Browserify
+        module.exports = factory;
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var supportType = ('onwheel' in document || document.documentMode >= 9) ?
+        // Modern browsers support "wheel" and IE9+
+        'wheel' :
+            document.onmousewheel !== undefined ?
+        // Webkit and IE support at least "mousewheel"
+        'mousewheel' :
+        // let's assume that remaining browsers are older Firefox
+        'DOMMouseScroll';
+
+    var props = 'deltaMode deltaX deltaY deltaZ detail';
+
+    console.log(supportType);
+
+    var wheel = $.event.special.wheel = {
+
+        setup: function () {
+            if (this.addEventListener) {
+                this.addEventListener(supportType, handle, false);
+            }
+        },
+
+        teardown: function () {
+            if (this.removeEventListener) {
+                this.removeEventListener(supportType, handle, false);
+            }
+        }
+    };
+
+    var handle = function (originalEvent) {
+        var event = $.event.fix(originalEvent),
+            args = arguments;
+
+        $.each(props.split(' '), function (index, prop) {
+            if (prop in originalEvent) {
+                event[prop] = originalEvent[prop];
+            }
+        });
+
+        args[0] = event;
+
+        return $.event.dispatch.apply(this, args);
+    };
+
+    jQuery.fn.wheel = function (fn) {
+        return (typeof fn === 'function') ? this.on('wheel', fn) : this.trigger('wheel', fn);
+    };
+}));
